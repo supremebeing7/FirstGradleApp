acaffb869118b09c0eefafa53192f114e81db891
Mark Lehman <markopolo@gmail.com>
2015-08-24 22:37:59 -0700

    setup velocity templating

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,44 +1,18 @@
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
 import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
-    get("/hello", (request, response) ->
-      "<!DOCTYPE html>" +
-        "<html>" +
-          "<head>" +
-            "<title>Hello Friend!</title>" +
-            "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-          "</head>" +
-          "<body>" +
-            "<h1>Hello From Afar</h1>" +
-            "<p>Dear Friend,</p>" +
-            "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-            "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-            "<p>But I like programming a lot, so I've got that going for me. </p>" +
-            "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-            "<p>Cheers,</p>" +
-            "<p>Travel Enthusiast Jane</p>" +
-          "</body>" +
-        "</html>"
-    );
-    get("/favorite_photos", (request, response) ->
-      "<!DOCTYPE html>" +
-        "<html>" +
-        "<head>" +
-          "<title>Hello Friend!</title>" +
-          "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-        "</head>" +
-        "<body>" +
-          "<h1>Favorite Traveling Photos</h1>" +
-          "<ul>" +
-            "<li><img src='/images/butter.png'/></li>" +
-            "<li><img src='/images/korea.png' alt='One does not simply come from North Korea'/></li>" +
-            "<li><img src='/images/sloths.jpg' alt='sloths are stupid.'/></li>" +
-            "<li><img src='/images/woof.jpg'/></li>" +
-          "</ul>" +
-        "</body>" +
-        "</html>"
-    );
+    get("/", (request, response) -> {
+      return new ModelAndView(new HashMap(), "templates/hello.vtl");
+    }, new VelocityTemplateEngine());
+    get("/favorite_photos", (request, response) -> {
+      return new ModelAndView(new HashMap(), "templates/favorite_photos.vtl");
+    }, new VelocityTemplateEngine());
   }
 }
